2583e6b424f86674ea2d8a80abedd70c0ef1dd62
Conall Kavanagh <conall.kavanagh76@mail.dcu.ie>
2023-06-12 09:53:48 +0100

    created an initial page with a route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	export const prerender = true;
+</script>
+
+<h1>Conall Kavanagh</h1>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/projects">Projects</a>
